464e56a26677051af9eba439a594986a368635d1
Sherzod <sherzodm8441@hstat.org>
2018-12-03 00:22:38 +0000

    caesar.rb

---
+++ lib/caesar.rb
@@ -0,0 +1,43 @@
+def caesar_decode(string, offset)
+    alpha_low = [*('a'..'z')]
+    alpha_up = [*('A'..'Z')]
+    
+    str = string.split("")
+    str.collect do |x|
+    if (alpha_low.index(x) != nil || alpha_up.index(x) != nil)
+        if x == x.downcase
+            alpha_low[(alpha_low.index(x).to_i - offset.to_i)%26]
+        elsif x == x.upcase
+            alpha_up[(alpha_up.index(x).to_i - offset.to_i)%26]
+        else
+            x
+        end
+    else
+    x
+    end
+    
+    
+    end.join
+end
+
+def caesar_encode(string, offset)
+    alpha_low = [*('a'..'z')]
+    alpha_up = [*('A'..'Z')]
+    
+    str = string.split("")
+    str.collect do |x|
+    if (alpha_low.index(x) != nil || alpha_up.index(x) != nil)
+        if x == x.downcase
+            alpha_low[(alpha_low.index(x).to_i + offset.to_i)%26]
+        elsif x == x.upcase
+            alpha_up[(alpha_up.index(x).to_i + offset.to_i)%26]
+        else
+            x
+        end
+    else
+    x
+    end
+    
+    
+    end.join
+end
